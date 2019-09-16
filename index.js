62bd7d4ce5841744ae6fbe9acde498c6d30b5813
Serjo <49415556+sergiomascarpone@users.noreply.github.com>
2019-09-16 20:53:28 +0300

    Add files via upload
    продолжение с переходом по / и Слову

--- index.js
+++ index.js
@@ -1,102 +1,17 @@
-import React, { Component } from 'react';
-import * as S from './styled.js'
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from '../modules/Login';
+import Dashboard from '../modules/Dashboard/';
 
-
-class App extends Component {
-  state = {
-    email: '',
-    password: '',
-    errors: {
-      email: '',
-      password: '',
-    }
-  };
-
-  validate = (event) => {
-    const valueKey = event.target.id;
-    const value = event.target.value;
-    const errors = {};
-
-    if (valueKey === 'password') {
-      if (value.length < 5) {
-        errors.password = 'password too short';
-      } else {
-        errors.password = ''
-      }
-    }
-
-    if (valueKey === 'email') {
-      if (value.includes('@')) {
-        errors.email = '';
-      } else {
-        errors.email = 'wrong email';
-      }
-    }
-    return errors;
-  }
-
-  handleInptChange = (e) => {
-    const event = e;
-    const update = {};
-    update[event.target.id] = event.target.value;
-
-    update.errors = this.validate(event);
-    this.setState(update);
-  }
-  render() {
-    const {
-      email,
-      password,
-      errors,
-    } = this.state;
+const AppRouter = () => {
     return (
-      <S.Container>
-        <div className='App'>
-          <div className="Body">
-            <S.Title>Login</S.Title>
+        <Router>
 
-            <div className='inptEmail'>
+            <Route exact path="/" component={Login} />
+            <Route path="/dashboard" component={Dashboard} />
 
-              <S.Label error={false}>
-                Email
-                </S.Label>
-
-              <input
-                className={errors.email ? 'errors' : ''}
-                id='email'
-                value={email}
-                placeholder='Email'
-                type='text'
-                onChange={this.handleInptChange}
-              />
-
-              {errors.email && <span>{errors.email}</span>}
-
-            </div>
-            <div className='inptPassword'>
-
-              <S.Label>Password</S.Label>
-
-              <input
-                className={errors.password ? 'errors' : ''}
-                id='password'
-                value={password}
-                placeholder='Password'
-                type='password'
-                onChange={this.handleInptChange}
-              />
-              {errors.password && <span>{errors.password}</span>}
-
-            </div>
-            <div>
-              <button>LogIn</button>
-            </div>
-          </div>
-        </div >
-      </S.Container>
+        </Router>
     )
-  }
+
 }
-
-
-export default App;
+export default AppRouter;
